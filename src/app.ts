3e0734b4fcedb8362b9b6a4fb748884bf311b870
lucasnather <natherlucas@gmail.com>
2024-02-06 12:21:10 -0400

    feat: conectando rota get poll com a aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 export const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
