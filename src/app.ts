6b1648e943aa1daf18ebb1a1339503abc4290113
lucasnather <natherlucas@gmail.com>
2024-02-06 12:24:11 -0400

    feat: configurando cookies

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 
 export const app = fastify()
+
+app.register(cookie, {
+    secret: 'talvez-seja-secreta',
+    hook: 'onRequest'
+})
 
 app.register(createPoll)
 app.register(getPoll)
