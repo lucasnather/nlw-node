ac78853484b209f37bad1690afbf58311c9f86e0
lucasnather <natherlucas@gmail.com>
2024-02-08 14:53:01 -0400

    feat: pub/sub para votação das enquetes

--- src/ws/routes/vote-results.ts
+++ src/ws/routes/vote-results.ts
@@ -3,8 +3,6 @@ import { FastifyInstance } from "fastify";
 export async function voteResults(app: FastifyInstance) {
 
     app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
-        connection.socket.on('message', message => {
-            connection.socket.send('hi from server' + message)
-        })
+       
     })
 }
