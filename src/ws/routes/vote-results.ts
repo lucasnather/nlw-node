b2d89a8188c5d3c800d271a71fe1643f2f0789d6
lucasnather <natherlucas@gmail.com>
2024-02-08 14:59:37 -0400

    feat: conectando websocket com resultado das enquetes

--- src/ws/routes/vote-results.ts
+++ src/ws/routes/vote-results.ts
@@ -1,8 +1,19 @@
 import { FastifyInstance } from "fastify";
+import { voting } from "../../utils/voting-pub-sub";
+import z from "zod";
 
 export async function voteResults(app: FastifyInstance) {
 
     app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
-       
+
+        const getParamsSchema = z.object({
+            pollId: z.string().uuid()
+        })
+
+        const { pollId } = getParamsSchema.parse(request.params)
+        
+        voting.subscribe(pollId, (message) => {
+            connection.socket.send(JSON.stringify(message))
+        })
     })
 }
